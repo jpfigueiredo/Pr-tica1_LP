405fc1311579de0ae79974f9f24989319099b7a1
jpfigueiredo <102195109+jpfigueiredo@users.noreply.github.com>
2022-04-06 20:48:54 -0300

    Questão 3 - Atividade prática 1

---
+++ pratica1_questao3/src/pratica1_questao3/vogais.java
@@ -0,0 +1,50 @@
+package pratica1_questao3;
+import java.util.Scanner;
+
+public class vogais {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner entrada = new Scanner (System.in);
+		
+		int numVogal = 0;
+		String frase;
+		
+		System.out.println("Digite uma frase: ");
+		frase = entrada.nextLine();
+		
+		//converte para maiusculo para simplificar a busca
+		frase = frase.toUpperCase();
+		String vogais="";
+		char c;
+		for(int i=0; i < frase.length(); i++)
+			{
+				c=frase.charAt(i);
+				if(c == 'A' || c == 'E' || c =='I' || c == 'O' || c == 'U')
+				{
+					vogais=vogais+Character.toString(c);
+					System.out.println(vogais);
+				}
+			}
+				
+		if (vogais.contains("A"))
+			numVogal ++;			
+		
+		if (vogais.contains("E"))
+			numVogal ++;
+		
+		if (vogais.contains("I"))
+			numVogal ++;
+		
+		if (vogais.contains("O"))
+			numVogal ++;
+		
+		if (vogais.contains("U"))
+			numVogal ++;
+			
+
+		System.out.println("A frase : " + frase + " possui " + numVogal + " vogal(is)");
+			
+	}
+
+}
